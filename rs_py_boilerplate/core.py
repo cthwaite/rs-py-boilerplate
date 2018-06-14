1c1ea22f3bc94871bca895c9ca03f58db02eaa14
cthwaite <thwaite@mac.com>
2018-06-14 20:13:00 +0100

    Add warning when falling back to pure Python module

--- rs_py_boilerplate/core.py
+++ rs_py_boilerplate/core.py
@@ -5,4 +5,7 @@
 try:
     from ._core import sum_as_string
 except ImportError:
+    import warnings
+    warnings.warn('Falling back on pure Python `sum_as_string`', RuntimeWarning)
+
     from .pure import sum_as_string
